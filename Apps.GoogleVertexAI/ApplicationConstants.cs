aa625e625f46b0d27ce5039751b3063d25e74091
sokolenkoolha <olha.sokolenko@blackbird.io>
2024-02-26 14:20:33 +0200

    made application constants class non-static

--- Apps.GoogleVertexAI/ApplicationConstants.cs
+++ Apps.GoogleVertexAI/ApplicationConstants.cs
@@ -1,6 +1,6 @@
 ﻿namespace Apps.GoogleVertexAI;
 
-public static class ApplicationConstants
+public class ApplicationConstants
 {
     public const string ClientId = "#{GOOGLEVERTEXAI_CLIENT_ID}#";
     public const string ClientSecret = "#{GOOGLEVERTEXAI_CLIENT_SECRET}#";
