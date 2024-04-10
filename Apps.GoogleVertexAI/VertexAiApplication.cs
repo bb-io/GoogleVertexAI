213fd17cb31987a5e23608ec85e715f8420e7193
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-10 11:41:15 +0300

    added categories

--- Apps.GoogleVertexAI/VertexAiApplication.cs
+++ Apps.GoogleVertexAI/VertexAiApplication.cs
@@ -1,9 +1,19 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.GoogleVertexAI;
 
-public class VertexAiApplication : IApplication
+public class VertexAiApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get =>
+        [
+            ApplicationCategory.ArtificialIntelligence, ApplicationCategory.Multimedia, ApplicationCategory.GoogleApps
+        ];
+        set { }
+    }
+
     public string Name
     {
         get => "Google Vertex AI";
