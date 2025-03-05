1692c3ee4ea638d6a79b35e145fe8850059e66fb
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-03-05 15:26:23 +0100

    Update Gemini Pro 2 name

--- Apps.GoogleVertexAI/DataSourceHandlers/Static/AIModelWithFileDataSourceHandler .cs
+++ Apps.GoogleVertexAI/DataSourceHandlers/Static/AIModelWithFileDataSourceHandler .cs
@@ -8,7 +8,7 @@ namespace Apps.GoogleVertexAI.DataSourceHandlers.Static
         private static Dictionary<string, string> Data = new Dictionary<string, string> {
                 {"gemini-2.0-flash","Gemini 2.0 Flash" } ,
                 {"gemini-2.0-flash-lite","Gemini 2.0 Flash-Lite" },
-                {"gemini-2.0-pro-exp-02-05", " Gemini 2.0 Pro " },
+                {"gemini-2.0-pro-exp-02-05", " Gemini 2.0 Pro Experimental 02-05" },
                 {"gemini-1.5-flash","Gemini 1.5 Flash"},
                 {"gemini-1.0-pro-vision","Gemini 1.0 Pro Vision"}
             };
