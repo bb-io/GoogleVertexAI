cd3c1b378646eadb86f9378db9c9bf67847a8237
mathijs-bb <mathijs.sonnemans@blackbird.io>
2025-07-11 11:55:15 +0200

    Updated available models

--- Apps.GoogleVertexAI/DataSourceHandlers/Static/AIModelDataSourceHandler.cs
+++ Apps.GoogleVertexAI/DataSourceHandlers/Static/AIModelDataSourceHandler.cs
@@ -7,18 +7,12 @@ public class AIModelDataSourceHandler : IStaticDataSourceItemHandler
 {
     private static readonly Dictionary<string, string> Data = new()
     {
-        { "gemini-2.5-flash-preview-04-17","Gemini 2.5 Flash Preview 04-17" },
-        { "gemini-2.5-pro-exp-03-25","Gemini 2.5 Pro Experimental 03-25" },
-        { "gemini-2.5-pro-preview-03-25","Gemini 2.5 Pro Preview 03-25" },
+        { "gemini-2.5-flash","Gemini 2.5 Flash" },
+        { "gemini-2.5-pro","Gemini 2.5 Pro" },
         { "gemini-2.0-flash","Gemini 2.0 Flash" },
         { "gemini-2.0-flash-lite","Gemini 2.0 Flash-Lite" },
-        { "gemini-2.0-pro-exp-02-05", " Gemini 2.0 Pro Experimental 02-05" },
+        { "gemini-1.5-flash","Gemini 1.5 Flash" },
         { "gemini-1.5-pro","Gemini 1.5 Pro" },
-        { "gemini-1.0-pro","Gemini 1.0 Pro" },
-        { "gemini-2.5-pro-preview-05-06", "Gemini 2.5 Pro Preview 05-06" },
-        { "gemini-2.5-flash-preview-05-20", "Gemini 2.5 Flash Preview 05-20" },
-        { "gemini-2.0-flash-001", "Gemini 2.0 Flash 001" },
-        { "gemini-2.0-flash-lite-001", "Gemini 2.0 Flash-Lite 001" }
     };
     
     public IEnumerable<DataSourceItem> GetData()
