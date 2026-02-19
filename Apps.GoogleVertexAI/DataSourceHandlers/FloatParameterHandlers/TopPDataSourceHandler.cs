13f91485be586f3ba71e091a0519a17f686e4647
mathijs-bb <mathijs.sonnemans@blackbird.io>
2026-02-19 18:33:45 +0100

    Added labels for temperature dropdown

--- Apps.GoogleVertexAI/DataSourceHandlers/FloatParameterHandlers/TopPDataSourceHandler.cs
+++ Apps.GoogleVertexAI/DataSourceHandlers/FloatParameterHandlers/TopPDataSourceHandler.cs
@@ -1,14 +1,19 @@
-﻿using Apps.GoogleVertexAI.DataSourceHandlers.FloatParameterHandlers.Base;
-using Blackbird.Applications.Sdk.Common.Invocation;
+﻿using Blackbird.Applications.Sdk.Common.Dynamic;
+using System.Globalization;
 
 namespace Apps.GoogleVertexAI.DataSourceHandlers.FloatParameterHandlers;
 
-public class TopPDataSourceHandler : BaseFloatParameterDataSourceHandler
+public class TopPDataSourceHandler : IDataSourceItemHandler
 {
-    protected override float LowerBoundary => 0.0f;
-    protected override float UpperBoundary => 1.0f;
-
-    public TopPDataSourceHandler(InvocationContext invocationContext) : base(invocationContext)
+    public IEnumerable<DataSourceItem> GetData(DataSourceContext context)
     {
+        return new DataSourceItem[]
+        {
+            new DataSourceItem(0.1f.ToString("0.00", CultureInfo.InvariantCulture), "0.1 | Governed"),
+            new DataSourceItem(0.3f.ToString("0.00", CultureInfo.InvariantCulture), "0.3 | Balanced"),
+            new DataSourceItem(0.5f.ToString("0.00", CultureInfo.InvariantCulture), "0.5 | Expressive"),
+            new DataSourceItem(0.6f.ToString("0.00", CultureInfo.InvariantCulture), "0.6 | Exploratory"),
+            new DataSourceItem(0.8f.ToString("0.00", CultureInfo.InvariantCulture), "0.8 | Experimental"),
+        };
     }
 }
