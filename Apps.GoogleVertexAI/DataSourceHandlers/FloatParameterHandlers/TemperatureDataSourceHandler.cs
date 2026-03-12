e9411d5ec490076a250ac56ead2fba35573f8243
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-12 16:52:27 +0200

    revert syntax changes in TemperatureDataSourceHandler

--- Apps.GoogleVertexAI/DataSourceHandlers/FloatParameterHandlers/TemperatureDataSourceHandler.cs
+++ Apps.GoogleVertexAI/DataSourceHandlers/FloatParameterHandlers/TemperatureDataSourceHandler.cs
@@ -1,5 +1,5 @@
-﻿using System.Globalization;
-using Blackbird.Applications.Sdk.Common.Dynamic;
+﻿using Blackbird.Applications.Sdk.Common.Dynamic;
+using System.Globalization;
 
 namespace Apps.GoogleVertexAI.DataSourceHandlers.FloatParameterHandlers;
 
@@ -7,13 +7,13 @@ public class TemperatureDataSourceHandler : IDataSourceItemHandler
 {
     public IEnumerable<DataSourceItem> GetData(DataSourceContext context)
     {
-        return
-        [
+        return new DataSourceItem[]
+        {
             new DataSourceItem(0.1f.ToString("0.00", CultureInfo.InvariantCulture), "0.1 | Governed"),
             new DataSourceItem(0.3f.ToString("0.00", CultureInfo.InvariantCulture), "0.3 | Balanced"),
             new DataSourceItem(0.5f.ToString("0.00", CultureInfo.InvariantCulture), "0.5 | Expressive"),
             new DataSourceItem(0.6f.ToString("0.00", CultureInfo.InvariantCulture), "0.6 | Exploratory"),
             new DataSourceItem(0.8f.ToString("0.00", CultureInfo.InvariantCulture), "0.8 | Experimental"),
-        ];
+        };
     }
 }
