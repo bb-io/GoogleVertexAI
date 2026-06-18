996934132a40aa90f7834fc88a3c73cce881e466
Alex Terehov <terales@users.noreply.github.com>
2026-06-18 05:42:01 -0230

    Commit missing files

---
+++ Apps.GoogleVertexAI/Models/Dto/ShortenCandidate.cs
@@ -0,0 +1,32 @@
+﻿using Blackbird.Filters.Extensions;
+using Blackbird.Filters.Transformations;
+using System.Globalization;
+
+namespace Apps.GoogleVertexAI.Models.Dto;
+
+public class ShortenCandidate
+{
+    public int Id { get; }
+    public Unit Unit { get; }
+    public List<Segment> Segments { get; }
+    public int MaximumGraphemes { get; }
+    public List<string> WorkingTargets { get; set; }
+    public List<string>? AcceptedTargets { get; set; }
+    public string? LastError { get; set; }
+
+    public int CurrentGraphemeCount => CountGraphemes(string.Concat(WorkingTargets));
+
+    public string DisplayId => Unit.Id ?? Id.ToString(CultureInfo.InvariantCulture);
+
+    public ShortenCandidate(int id, Unit unit, int maximumGraphemes)
+    {
+        Id = id;
+        Unit = unit;
+        Segments = unit.Segments.Where(segment => !segment.IsIgnorbale).ToList();
+        MaximumGraphemes = maximumGraphemes;
+        WorkingTargets = Segments.Select(segment => segment.GetTarget()).ToList();
+    }
+
+    private static int CountGraphemes(string? value)
+        => StringInfo.ParseCombiningCharacters(value ?? string.Empty).Length;
+}
